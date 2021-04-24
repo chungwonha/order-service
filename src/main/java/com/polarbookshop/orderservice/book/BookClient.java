79cf96ebce7f45d914019523ca90cd8ce98bb973
chungha@yahoo.com <chungha@yahoo.com>
2021-04-24 15:38:02 -0400

    8.4.2 changes

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -3,6 +3,7 @@ package com.polarbookshop.orderservice.book;
 import reactor.core.publisher.Mono;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 
@@ -17,10 +18,16 @@ public class BookClient {
                 .build();
     }
 
+    /*
+    Project Reactor provides a retry() operator to retry an operation in case it fails.
+    You can add it to the reactive stream after the timeout() operator. The time limiter is applied first.
+    If the timeout expires, the retry() operator kicks in and tries the request again.
+     */
     public Mono<Book> getBookByIsbn(String isbn) {
         return webClient.get().uri(isbn)
                 .retrieve()
                 .bodyToMono(Book.class)
-                .timeout(Duration.ofSeconds(1),Mono.empty());
+                .timeout(Duration.ofSeconds(1), Mono.empty())  //timeout operator
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100))); //retry with backoff when timeout occurs
     }
 }
